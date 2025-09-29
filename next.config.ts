55466da8224b7881044a6e610a3a0e20445cf118
surajkrprashad-dev <suraj.kumar.prashad.dev@gmail.com>
2025-09-29 18:18:11 +0530

    Fix TypeScript types, use next/image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+        port: "",
+        pathname: "/vi/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
